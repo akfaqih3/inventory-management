8b3ad79e24d00dc941781c24bab4925d8c6b926c
akfaqih3 <akfaqih3@gmail.com>
2025-12-25 13:14:11 -0800

    fix: align of filter drowpdown

--- src/app/features/product/components/product-table-filter/product-table-filter.ts
+++ src/app/features/product/components/product-table-filter/product-table-filter.ts
@@ -1,7 +1,8 @@
 import { required } from '@angular/forms/signals';
 import { CategoryModel } from './../../models/category-model';
-import { Component, Input, input, output, Output } from '@angular/core';
+import { Component, inject, Input, input, output, Output } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { LanguageService } from '../../../../core/i18n/service/language.service';
 
 @Component({
   selector: 'app-product-table-filter',
@@ -10,6 +11,10 @@ import { TranslateModule } from '@ngx-translate/core';
   styleUrl: './product-table-filter.css',
 })
 export class ProductTableFilter {
+
+  private _languageService = inject(LanguageService);
+
+  readonly isRTL = this._languageService.isRTL;
 
   categories = input<CategoryModel[]>();
 
